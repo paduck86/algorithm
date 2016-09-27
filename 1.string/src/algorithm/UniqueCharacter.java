30673eb7f0f91d8b3d1725f718e117723301071c
paduck86 <paduck86@gmail.com>
2016-09-28 08:57:09 +0900

    string

--- 1.string/src/algorithm/UniqueCharacter.java
+++ 1.string/src/algorithm/UniqueCharacter.java
@@ -6,6 +6,7 @@ public class UniqueCharacter {
 		boolean isUnique = true;
 		for (int i = 0; i < length; i++) {
 			for (int j = i + 1; j < length; j++) {
+				int k = str.charAt(i);
 				if(str.charAt(j) == str.charAt(i)) {
 					isUnique = false;
 					break;
@@ -13,6 +14,21 @@ public class UniqueCharacter {
 			}
 		}
 		return isUnique;
+	}
+	
+	public boolean isUniqueCharset_answer (String str) {
+		int length = str.length();
+		if(length > 256) return false;
+		boolean[] char_set = new boolean[256];
+		
+		for(int i = 0; i < length; i++) {
+			int val = str.charAt(i);
+			if(char_set[val]) {
+				return false;
+			}
+			char_set[val] = true;
+		}
+		return true;
 	}
 }
 
--- 1.string/src/test/UniqueCharacterTest.java
+++ 1.string/src/test/UniqueCharacterTest.java
@@ -9,13 +9,13 @@ public class UniqueCharacterTest {
 	@Test
 	public void test1() {
 		UniqueCharacter uc = new UniqueCharacter();
-		assertEquals("is not unique character set : ", false, uc.isUniqueCharacterSet("123111"));
+		assertEquals("is not unique character set : ", false, uc.isUniqueCharset_answer("123111"));
 	}
 	
 	@Test
 	public void test2() {
 		UniqueCharacter uc = new UniqueCharacter();
-		assertEquals("is unique character set : ", true, uc.isUniqueCharacterSet("123456"));
+		assertEquals("is unique character set : ", true, uc.isUniqueCharset_answer("123456"));
 	}
 
 }
