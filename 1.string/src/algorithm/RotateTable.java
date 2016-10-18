00bce657efb4962b7696abac807f5be163c0dcd0
paduck86 <paduck86@gmail.com>
2016-10-19 08:54:08 +0900

    rotate table

--- 1.string/src/algorithm/RotateTable.java
+++ 1.string/src/algorithm/RotateTable.java
@@ -13,4 +13,32 @@ public class RotateTable {
 		
 		return matrix;
 	}
+	
+	public int[][] rotate(int[][] matrix, int n) {
+		int [][] matrix2 = new int[n][n];
+		for (int layer = 0; layer < n/2; layer++) {
+			int first = layer;
+			int last = n - 1 - layer;
+			for(int i = first; i < last; i++) {
+				int offset = i - first;
+				
+				//위쪽을 저장
+				int top = matrix[first][i];
+				
+				//왼쪽 -> 위쪽
+				matrix[first][i] = matrix[last-offset][first];
+				
+				//아래 -> 왼쪽
+				matrix[last-offset][first] = matrix[last][last - offset];
+				
+				//오른쪽 -> 아래
+				matrix[last][last - offset] = matrix[i][last];
+				
+				//위 -> 오른쪽
+				matrix[i][last] = top;
+			}
+		}
+		System.out.println(matrix.toString());
+		return matrix;
+	}
 }
--- 1.string/src/test/RotateTableTest.java
+++ 1.string/src/test/RotateTableTest.java
@@ -8,26 +8,57 @@ import algorithm.RotateTable;
 
 public class RotateTableTest {
 	private RotateTable rt = new RotateTable();
-	int[][] table = {
+	int[][] table1 = {
 			{1,2,3,4},
 			{5,6,7,8},
 			{9,10,11,12},
 			{13,14,15,16}
 	};
-	int[][] table_rotated = {
+	int[][] table1_rotated = {
 			{13,9,5,1},
 			{14,10,6,2},
 			{15,11,7,3},
 			{16,12,8,4}
 	};
-	@Test
+	int[][] table2 = {
+			{1,2,3},
+			{4,5,6},
+			{7,8,9}
+	};
+	int[][] table2_rotated = {
+			{7,4,1},
+			{8,5,2},
+			{9,6,3}
+	};
+	/*@Test
 	public void test1() {
-		int[][] table_result = rt.solution(table);
+		int[][] table_result = rt.solution(table1);
 		
 		for(int i=0; i<table_result.length; i++) {
 			for(int j=0; j<table_result.length; j++) {
-				assertEquals("Rotates the table 90 degrees : ", table_rotated[i][j] , table_result[i][j]);	
+				assertEquals("Rotates the table 90 degrees : ", table1_rotated[i][j] , table_result[i][j]);	
 			}
+		}
+	}*/
+	@Test public void test2() {
+		int[][] table_result = rt.rotate(table1, 4);
+		for(int i=0; i<table_result.length; i++) {
+			for(int j=0; j<table_result.length; j++) {
+				System.out.print(table_result[i][j] + ",");
+				//assertEquals("Rotates the table 90 degrees : ", table1_rotated[i][j] , table_result[i][j]);
+			}
+			System.out.println();
+		}
+	}
+	@Test public void test3() {
+		System.out.println(table2.toString());
+		int[][] table_result = rt.rotate(table2, 3);
+		for(int i=0; i<table_result.length; i++) {
+			for(int j=0; j<table_result.length; j++) {
+				System.out.print(table_result[i][j] + ",");
+				//assertEquals("Rotates the table 90 degrees : ", table2_rotated[i][j] , table_result[i][j]);	
+			}
+			System.out.println();
 		}
 	}
 }
