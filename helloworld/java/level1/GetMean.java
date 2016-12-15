9250a859f7d508e3d9df2957930040bcab2ccbe3
paduck86 <paduck86@gmail.com>
2016-12-15 09:11:30 +0900

    java helloworld

--- helloworld/java/level1/GetMean.java
+++ helloworld/java/level1/GetMean.java
@@ -10,7 +10,7 @@ public class GetMean {
     }*/
 	
 	public int getMean(int[] array) {
-		if(array == null) {
+		if(array == null || array.length == 0) {
 			return 0;
 		}
 		
@@ -23,6 +23,7 @@ public class GetMean {
 	
     public static void main(String[] args) {
         int x[] = {5, 4, 3};
+    	
         GetMean getMean = new GetMean();
         // 아래는 테스트로 출력해 보기 위한 코드입니다.
         System.out.println("평균값 : " + getMean.getMean(x));
